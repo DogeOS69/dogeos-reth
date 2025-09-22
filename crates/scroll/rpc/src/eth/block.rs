d590bf058e75d7e327da7098f05fbaa12d2098bb
greg <82421016+greged93@users.noreply.github.com>
2025-09-22 17:40:44 +0200

    feat: add missing td to rpc header (#342)
    * feat: add missing td to rpc header
    
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>
    
    * fix: lints
    
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>
    
    ---------
    
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/scroll/rpc/src/eth/block.rs
+++ crates/scroll/rpc/src/eth/block.rs
@@ -1,11 +1,14 @@
 //! Loads and formats Scroll block RPC response.
 
-use crate::{ScrollEthApi, ScrollEthApiError};
+use crate::{RpcBlockHeaderMut, ScrollEthApi, ScrollEthApiError};
 
-use reth_rpc_convert::RpcConvert;
+use alloy_consensus::BlockHeader;
+use alloy_eips::BlockId;
+use reth_provider::HeaderProvider;
+use reth_rpc_convert::{RpcConvert, RpcTypes};
 use reth_rpc_eth_api::{
     helpers::{EthBlocks, LoadBlock},
-    RpcNodeCore,
+    EthApiTypes, FromEthApiError, FullEthApiTypes, RpcBlock, RpcNodeCore,
 };
 use reth_rpc_eth_types::error::FromEvmError;
 
@@ -14,7 +17,33 @@ where
     N: RpcNodeCore,
     ScrollEthApiError: FromEvmError<N::Evm>,
     Rpc: RpcConvert<Primitives = N::Primitives, Error = ScrollEthApiError>,
+    <<Self as EthApiTypes>::NetworkTypes as RpcTypes>::Header: RpcBlockHeaderMut,
 {
+    async fn rpc_block(
+        &self,
+        block_id: BlockId,
+        full: bool,
+    ) -> Result<Option<RpcBlock<Self::NetworkTypes>>, Self::Error>
+    where
+        Self: FullEthApiTypes,
+    {
+        let Some(block) = self.recovered_block(block_id).await? else { return Ok(None) };
+
+        let td = self
+            .provider()
+            .header_td_by_number(block.number())
+            .map_err(Self::Error::from_eth_err)?;
+
+        let mut block = block.clone_into_rpc_block(
+            full.into(),
+            |tx, tx_info| self.tx_resp_builder().fill(tx, tx_info),
+            |header, size| self.tx_resp_builder().convert_header(header, size),
+        )?;
+
+        *block.header.total_difficulty_mut() = td;
+
+        Ok(Some(block))
+    }
 }
 
 impl<N, Rpc> LoadBlock for ScrollEthApi<N, Rpc>
